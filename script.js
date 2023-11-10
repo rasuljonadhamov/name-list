09ceb9f602a7495b3ec733532a379857f208b4a4
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-10 18:37:33 +0300

    Add functionality to save localStorage

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
-let car = {
-  name: "Mers",
-  year: 2023,
-  color: "Black",
-};
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+const email = document.getElementById("email");
+const btn = document.getElementById("btn");
+const form = document.getElementById("form-wrapper");
 
-let { name: model, weight = 2000, ...cars } = car;
-console.log(weight, cars);
+// ozgaruvchi
 
-function sum(name, surname, ...args) {
-  let sum = 0;
+let user = {};
 
-  args.forEach((el) => (sum += el));
+// functions
+function checkValues() {
+  function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
+    }
+    alert("You have entered an invalid email address!");
+    return false;
+  }
 
-  return sum;
+  ValidateEmail(email.value);
+
+  if (!name.value) {
+    name.style.outlineColor = "red";
+    name.focus();
+    return;
+  }
+
+  if (!age.value) {
+    age.style.outlineColor = "red";
+    age.focus();
+    return;
+  }
+
+  if (!email.value) {
+    email.style.outlineColor = "red";
+    email.focus();
+    return;
+  }
 }
 
-console.log(sum("Rasuljon", "Adhamov", 3, 4, 5));
+function saveToLoacalStorage() {
+  let data = localStorage.getItem("users")
+    ? JSON.parse(localStorage.getItem("users"))
+    : [];
+
+  user.id = data.length;
+  user.name = name.value;
+  user.age = age.value;
+  user.email = email.value;
+
+  data.push(user);
+
+  localStorage.setItem("users", JSON.stringify(data));
+}
+
+// Events
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  checkValues();
+  saveToLoacalStorage();
+
+  form.reset();
+});
